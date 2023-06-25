954405f742d5e8f59a13768a72350f1e0dfaf0f6
Euris <55666525+Euris14@users.noreply.github.com>
2023-06-24 21:58:47 -0400

    Update fluidd.py
    general updating

--- fluidd.py
+++ fluidd.py
@@ -6,9 +6,10 @@ from flask import Flask, render_template
 import os
 
 
-def main():  # this is the main function were I call all functions. 
+def main():# this is the main function were I call all functions.
 
-    for i in range(10): # this loop checks to see if the getServerInfo function works, if it throws an exception, than I can asume that the server is down, or the ip address is typed incorrectly.
+    
+    for i in range(10):# this loop checks to see if the getServerInfo function works, if it throws an exception, than I can asume that the server is down, or the ip address is typed incorrectly.
         user_ip = input("Please input your servers ip address (10.7.1.1): ")
         ip = f'http://{user_ip}'
         try:
@@ -17,19 +18,15 @@ def main():  # this is the main function were I call all functions.
         except:
             print("Server is down.")
         print(f"Could not connect. {i}/10.")
-         
     serverCheckServices(ip)
-    # this for loop checks if the getServerInfo function is able to get serverstats, if it can't then it wait 10 seconds.
-    for i in range(2):
+    for i in range(2):# this for loop checks if the getServerInfo function is able to get serverstats, if it can't then it wait 10 seconds.
         try:
             server_stats = getServerInfo(ip)
             break
         except:
             print("Critical services might be down, letting them restart.")
             time.sleep(10)
-
-    
-    for c in server_stats: #this for loop just prints the serverstats in succession.
+    for c in server_stats:# this for loop just prints the serverstats in succession.
         match c:
             case "host_ip":
                 print(f'\nHost IP: {server_stats[c]}')
@@ -43,39 +40,29 @@ def main():  # this is the main function were I call all functions.
                 print(f'SD Card Manufacturer: {server_stats[c]}')
             case "sd_size":
                 print(f'SD Card Size: {server_stats[c]}')
-
+    printer_status = server_stats['printer_state']
     
-    printer_status = klipperStatus(server_stats['printer_state'])
-
-    while not printer_status:
-        print(f"Printer state is '{server_stats['printer_state']}', retrying every 8 mins, until printer is 'ready'.")
+    while printer_status != 'ready':
+        print("Printer is not ready!")
+        if printer_status == 'shutdown':
+            print(f"Printer status is currently: {printer_status}.")
+        elif printer_status == 'startup':
+            print(f"Printer status is currently: {printer_status}.")
+        elif printer_status == 'error':
+            print(f"Printer status is currently: {printer_status}.")
         restartFirmware(ip)
         time.sleep(8)
-    else:
-        print("Printer has connected.", end='\r')
+    print("Printer Connected.")
 
-    
-        
-def displayTools(ip): # this function is meant to be initiated and shown across.
+def displayTools(ip):# this function is meant to be initiated and shown across.
     tools = getTools(ip)
     for tool in tools:
         tool_temp = toolTemperature(tool, ip)
-
         print(f"{tool} : {tool_temp}")
     time.sleep(.8)
     os.system('cls')
-        
-def klipperStatus(klippy):# checks getServerInfo function for klipper state, if its not ready, then restart firmware.
-    if klippy != 'ready':
-        if klippy == 'shutdown':
-            return False
-        elif klippy == 'startup':
-            return False
-        elif klippy == 'error':
-            return False
-    return True
 
-def getTools(ip): #this function gets all available tools like in the temperature function.
+def getTools(ip):# this function gets all available tools like in the temperature function.
     response = rq.get(f"{ip}/server/temperature_store")
     json_resp = response.json()
     tools = []
@@ -107,7 +94,7 @@ def getServerInfo(ip):# this function gets server information, like cpu info and
     # Klipper state
     getResponse2 = rq.get(f"{ip}{get[1]}")  # gets reponse from /server/info
     json2 = getResponse2.json()
-    # stores state of klipper, and checks if klippy is connected.
+    # stores printer state, and checks if klipper is connected.
     if not json2['result']:
         printer_state = 'error'
         printer_message = json2['error']['message']
@@ -125,35 +112,30 @@ def getServerInfo(ip):# this function gets server information, like cpu info and
     information.update({'printer_state': printer_state})
     information.update({'printer_message': printer_message})
 
-    # returns the dictionary created by {"category":"data"}.
-    return information
+    
+    return information # returns the dictionary created by {"category":"data"}.
 
 
-# sends post request to restart the printer's firmware.
-def restartFirmware(ip):
+def restartFirmware(ip):# sends post request to restart the printer's firmware.
     response = rq.post(f'{ip}/printer/firmware_restart')
     post_response_json = response.json()
-    
-    try:  # checks to see if there is a result. eventually want to check if it restarted correctly and is now connected.
+    try:# checks to see if there is a result. eventually want to check if it restarted correctly and is now connected.
         json['result']
     except:
         return 'ok'
     return 'failed'
-    
 
-# this function checks the ~current temperature out of all devices. (Want to implement this better, maybe multithreading?)
-def toolTemperature(tool, ip):
+def toolTemperature(tool, ip):# this function checks the ~current temperature out of all devices. (Want to implement this better, maybe multithreading?)
     response = rq.get(f"{ip}/server/temperature_store")
     response_json = response.json()
 
     temperatures = response_json['result'][tool]['temperatures']
-    tool_temperature = response_json['result'][tool]['temperatures'][len(temperatures) - 1]
+    tool_temperature = response_json['result'][tool]['temperatures'][len(
+        temperatures) - 1]
 
     return tool_temperature
 
-
-# this function checks all of the services up on moonraker, and tries to restart them
-def serverCheckServices(ip):
+def serverCheckServices(ip):# this function checks all of the services up on moonraker, and tries to restart them
     response = rq.get(f"{ip}/machine/system_info")
     response_json = response.json()
     # this dictionary holds all of the available services no matter if they are running or not.
@@ -162,17 +144,15 @@ def serverCheckServices(ip):
     service = response_json["result"]["system_info"]["service_state"]
     active = []
     not_active = []
-    # for every service in all available services, check to see if they are running or not by check the services dict.
-    for c in availableServices:
+    
+    for c in availableServices:# for every service in all available services, check to see if they are running or not by check the services dict.
         serviceState = service[c]["active_state"]
-        # checks serviceState bool, if true, than add to active dict to be returned, else, be sent to not active.
-        if not serviceState == "active":
+        if not serviceState == "active":# checks serviceState bool, if true, than add to active dict to be returned, else, be sent to not active.
             not_active.append(c)
             print(f"{c} is down!")
             print(f'Restarting service now.')
-
             rq.post(f"http://10.7.1.215/machine/services/restart?service={c}")
-            
+
         elif serviceState:
             active.append(c)
             print(f"\r{c} is up and running!")
@@ -180,8 +160,5 @@ def serverCheckServices(ip):
 
     return availableServices, active, not_active
 
-
 if __name__ == "__main__":
     main()
-
-
